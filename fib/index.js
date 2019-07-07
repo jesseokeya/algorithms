ca5586f98a467fa77066c0daa342c75c38a230fb
jesseokeya <jesseokeya@gmail.com>
2019-07-06 20:48:39 -0400

    Update index.js

--- fib/index.js
+++ fib/index.js
@@ -8,6 +8,42 @@
 // Example:
 //   fib(4) === 3
 
-function fib(n) {}
+const slowRecursiveFibonacci = (n) => {
+    if (n < 2) {
+        return n
+    }
+    return fib(n - 1) + fib(n - 2)
+}
+
+const fastFibonnacci = (n) => {
+    const results = [0, 1]
+    for (let i = 2; i <= n; i++) {
+        const first = results[i - 1]
+        const second = results[i - 2]
+        results.push(first + second)
+    }
+    return results[n]
+}
+
+/**
+ * In computing, memoization or memoisation is an optimization technique used primarily to speed up
+ * computer programs by storing the results of expensive function calls and returning the cached result
+ * when the same inputs occur again.Memoization has also been used in other contexts(and for purposes other than speed gains),
+ * such as in simple mutually recursive descent parsing.[1] Although related to caching, memoization refers to a specific case of this optimization,
+ * distinguishing it from forms of caching such as buffering or page replacement.In the context of some logic programming languages, memoization is also known as tabling.
+ */
+const memoize = (fn) => {
+    const cache = {}
+    return (...args) => {
+        if (cache[args]) {
+            return cache[args]
+        }
+        const result = fn.apply(this, args) 
+        cache[args] = result
+        return result
+    }
+}
+
+const fib = memoize(slowRecursiveFibonacci)
 
 module.exports = fib;
