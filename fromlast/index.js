f6ab82789fac633f6fe9ee55b42f3d1b7e6dde0d
jesseokeya <jesseokeya@gmail.com>
2019-07-09 11:32:48 -0400

    Update index.js

--- fromlast/index.js
+++ fromlast/index.js
@@ -11,6 +11,21 @@
 //    list.insertLast('d');
 //    fromLast(list, 2).data // 'b'
 
-function fromLast(list, n) {}
+function fromLast(list, n) {
+    let fast = list.getFirst()
+    let slow = list.getFirst()
+
+    while(n > 0) {
+        fast = fast.next
+        n--
+    }
+
+    while (fast.next) {
+        fast = fast.next
+        slow = slow.next
+    }
+
+    return slow
+}
 
 module.exports = fromLast;
