43a20f0b09af9e4943a1ed25d5035bcced4fc5e8
jesseokeya <jesseokeya@gmail.com>
2019-07-09 23:17:45 -0400

    changes

--- tree/index.js
+++ tree/index.js
@@ -8,10 +8,45 @@
 // should initialize a 'root' property to null.
 // 3) Implement 'traverseBF' and 'traverseDF'
 // on the tree class.  Each method should accept a
-// function that gets called with each element in the tree
+// function that gets call4ed with each element in the tree
 
-class Node {}
+class Node {
+    constructor(data) {
+        this.data = data
+        this.children = []
+    }
 
-class Tree {}
+    add(data) {
+        this.children.push(new Node(data))
+    }
+
+    remove(data) {
+        this.children = this.children.filter(child => child.data !== data)
+    }
+}
+
+class Tree {
+  constructor() {
+    this.root = null;
+  }
+
+  traverseBF(fn) {
+    const arr = [this.root];
+    while (arr.length) {
+      const node = arr.shift();
+      arr.push(...node.children);
+      fn(node);
+    }
+  }
+
+  traverseDF(fn) {
+      const arr = [this.root]
+      while (arr.length) {
+          const node = arr.shift()
+          arr.unshift(...node.children)
+          fn(node)
+      }
+  }
+}
 
 module.exports = { Tree, Node };
