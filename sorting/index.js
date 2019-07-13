f53c6096fa4960ae5a2733efd0ddac17b8732769
jesseokeya <jesseokeya@gmail.com>
2019-07-13 06:29:07 -0400

    changes

--- sorting/index.js
+++ sorting/index.js
@@ -2,7 +2,16 @@
 // Implement bubbleSort, selectionSort, and mergeSort
 
 function bubbleSort(arr) {
-
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = 0; j < (arr.length - 1) - 1; j++) {
+            if (j + 1 < arr.length && arr[j] > arr[j + 1]) {
+                const temp = arr[j]
+                arr[j] = arr[j + 1]
+                arr[j + 1] = arr[j]
+            }
+        }
+    }
+    return arr
 }
 
 function selectionSort(arr) {
