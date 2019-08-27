05c39d866125649e43dfdccbbc8277c12db81df2
jesseokeya <jesseokeya@gmail.com>
2019-08-27 08:56:16 -0400

    Update index.js

--- levelwidth/index.js
+++ levelwidth/index.js
@@ -13,18 +13,18 @@
 
 function levelWidth(root) {
     const arr = [root, 's']
-    const counters = [0]
+    const counter = [0]
     while (arr.length > 1) {
         const node = arr.shift()
         if (node === 's') {
-            counters.push(0)
+            counter.push(0)
             arr.push('s')
         } else {
             arr.push(...node.children)
-            counters[counters.length - 1]++
+            counter[counter.length - 1]++
         }
     }
-    return counters
+    return counter
 }
 
 module.exports = levelWidth;
