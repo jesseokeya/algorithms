3cd4b529bca4aaea844b37eaabb95748e2c2f19f
Jesse Okeya <Jesseokeya@gmail.com>
2020-12-06 18:11:32 -0500

    working commit - Created using LeetHub

--- unique-paths/unique-paths.js
+++ unique-paths/unique-paths.js
@@ -1,15 +1,19 @@
-const memo = {}
-​
 /**
  * @param {number} m
  * @param {number} n
  * @return {number}
  */
 var uniquePaths = function(m, n) {
-    const key = `${m},${n}`
-    if (memo.hasOwnProperty(key)) return memo[key]
-    if (m === 1 && n === 1) return 1
-    if (m === 0 || n === 0) return 0
-    memo[key] = uniquePaths(m - 1, n) + uniquePaths(m, n - 1)
-    return memo[key]
+    const memo = {}
+    
+    const possiblePaths = (m, n) => {
+        const key = `${m},${n}`
+        if (memo.hasOwnProperty(key)) return memo[key]
+        if (m === 1 && n === 1) return 1
+        if (m === 0 || n === 0) return 0
+        memo[key] = possiblePaths(m - 1, n) + possiblePaths(m, n - 1)
+        return memo[key] 
+    }
+    
+    return possiblePaths(m, n)
 };
