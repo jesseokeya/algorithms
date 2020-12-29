c018a6c95d2ad355d1f4d682c7e043aa869f8f1f
Jesse Okeya <Jesseokeya@gmail.com>
2020-12-29 13:45:14 -0500

    Time: 96 ms (77.41%), Space: 41.4 MB (87.16%) - LeetHub

--- permutations-ii/permutations-ii.js
+++ permutations-ii/permutations-ii.js
@@ -3,22 +3,22 @@
  * @return {number[][]}
  */
 var permuteUnique = function(nums) {
-    const results = []
-    const visited = new Array(nums.length).fill(false)
-    nums.sort((a, b) => a - b);
+    nums.sort((a, b) => a - b)
     
-    const dfs = (nums, prefix, visited) => {
+    const visited = new Array(nums.length).fill(false), results = []
+    
+    const dfs = (prefix, visited) => {
         if (nums.length === prefix.length) return results.push(prefix)
         for (let i = 0; i < nums.length; i++) {
-            if (visited[i] || i > 0 && nums[i] === nums[i-1] && !visited[i-1]){
-                continue;
-            }
+            if (visited[i] || i > 0 && nums[i] === nums[i - 1] && !visited[i - 1]) continue
             visited[i] = true
-            dfs(nums, prefix.concat(nums[i]), visited)
+            dfs(prefix.concat(nums[i]), visited)
             visited[i] = false
         }
     }
     
-    dfs(nums, [], visited)
+    
+    dfs([], visited)
+    
     return results
 };
