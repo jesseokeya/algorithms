2c96a87c210859da6ce6e9a4a7c55ea11d509194
Jesse Okeya <Jesseokeya@gmail.com>
2021-01-16 19:30:56 -0500

    Time: 148 ms (75.49%), Space: 49 MB (44.85%) - LeetHub

---
+++ 3sum/3sum.js
@@ -0,0 +1,28 @@
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+var threeSum = function(nums) {
+    const results = []
+    if (nums.length < 3) return results
+    
+    nums.sort((a, b) => a - b)
+    
+    for (let i = 0; i < nums.length - 2; i++) {
+        let left = i + 1, right = nums.length - 1, sum = 0 - nums[i]
+        if (i >= 0 && nums[i] !== nums[i - 1]) {
+        while (left < right) {
+            if (nums[left] + nums[right] < sum) left++
+            else if (nums[left] + nums[right] > sum) right--
+            else {
+                results.push([nums[i], nums[left], nums[right]])
+                while(left < right && nums[left] === nums[left + 1]) left++
+                while(left < right && nums[right] === nums[right - 1]) right--
+                left++
+                right--
+            }
+            
+        }}
+    }
+    return results
+};
