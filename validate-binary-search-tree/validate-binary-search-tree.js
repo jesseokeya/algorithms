916e693aea200846b5bb1a049ebac285be86e521
Jesse Okeya <Jesseokeya@gmail.com>
2021-02-05 18:19:25 -0500

    Time: 80 ms (97.16%), Space: 42.8 MB (48.99%) - LeetHub

--- validate-binary-search-tree/validate-binary-search-tree.js
+++ validate-binary-search-tree/validate-binary-search-tree.js
@@ -3,11 +3,11 @@
  * @return {boolean}
  */
 var isValidBST = function(root) {
-    return validate(root, -Infinity, Infinity);
+    return validate(root, -Infinity, Infinity);
 };
-​
+
 const validate = (node, min, max) => {
-    if (!node) { return true }
-    if (node.val > max || node.val < min) { return false }
-    return validate(node.left, min, node.val - 1) && validate(node.right, node.val + 1, max) 
+    if (!node) { return true }
+    if (node.val > max || node.val < min) { return false }
+    return validate(node.left, min, node.val - 1) && validate(node.right, node.val + 1, max) 
 }
