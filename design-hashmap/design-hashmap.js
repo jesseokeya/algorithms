746aa5d9e2e2b122ffb77a40b860f6b60808f2c6
Jesse Okeya <Jesseokeya@gmail.com>
2021-03-07 08:42:08 -0500

    Time: 484 ms (6.02%), Space: 95.4 MB (5.66%) - LeetHub

---
+++ design-hashmap/design-hashmap.js
@@ -0,0 +1,45 @@
+/**
+ * Initialize your data structure here.
+ */
+var MyHashMap = function() {
+    const MAX_SIZE = 1000000
+    this.collection = new Array(MAX_SIZE).fill(-1)
+};
+
+/**
+ * value will always be non-negative. 
+ * @param {number} key 
+ * @param {number} value
+ * @return {void}
+ */
+MyHashMap.prototype.put = function(key, value) {
+     this.collection[key] = value
+};
+
+/**
+ * Returns the value to which the specified key is mapped, or -1 if this map contains no mapping for the key 
+ * @param {number} key
+ * @return {number}
+ */
+MyHashMap.prototype.get = function(key) {
+    const result = this.collection[key]
+    if (result > -1) return this.collection[key]
+    return -1
+};
+
+/**
+ * Removes the mapping of the specified value key if this map contains a mapping for the key 
+ * @param {number} key
+ * @return {void}
+ */
+MyHashMap.prototype.remove = function(key) {
+    this.collection[key]  = -1
+};
+
+/** 
+ * Your MyHashMap object will be instantiated and called as such:
+ * var obj = new MyHashMap()
+ * obj.put(key,value)
+ * var param_2 = obj.get(key)
+ * obj.remove(key)
+ */
