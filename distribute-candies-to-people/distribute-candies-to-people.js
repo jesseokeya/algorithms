8cea27fd4276d36ed1a6b59e5462ba311a141f49
Jesse Okeya <Jesseokeya@gmail.com>
2021-03-13 06:56:00 -0500

    Time: 84 ms (46.61%), Space: 38.8 MB (16.95%) - LeetHub

--- distribute-candies-to-people/distribute-candies-to-people.js
+++ distribute-candies-to-people/distribute-candies-to-people.js
@@ -4,17 +4,16 @@
  * @return {number[]}
  */
 var distributeCandies = function(candies, num_people) {
-    if (!candies || candies == 0) return []; //no candies
-    let res = new Array(num_people).fill(0); //aovid NaN
-    let candy = 1; //give first person
-    let i = 0; //tracking res array
-    while (candies > 0){
-        i = i % num_people; //for assign candy from left again
-        if (candies < candy) res[i] += candies;
-        else res[i] += candy;
-        i++;
-        candies -= candy;
-        candy++; //increasing candy amount
-    }  
-    return res;
+    if (!candies || candies == 0) return [];
+    let candy = 1, index = 0, result = new Array(num_people).fill(0)
+    while (candies > 0) {
+        index = index % num_people
+        if (candies < candy) result[index] += candies
+        else result[index] += candy
+        
+        index++
+        candies -= candy
+        candy++
+    }
+    return result
 };
