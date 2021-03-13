51f4fbfdca4b5be94b6bac4c06266adcb2c24ec9
Jesse Okeya <Jesseokeya@gmail.com>
2021-03-13 06:20:41 -0500

    Time: 88 ms (78.03%), Space: 43.2 MB (17.34%) - LeetHub

--- isomorphic-strings/isomorphic-strings.js
+++ isomorphic-strings/isomorphic-strings.js
@@ -4,18 +4,17 @@
  * @return {boolean}
  */
 var isIsomorphic = function(s, t) {
-   if (new Set(s).size !== new Set(t).size) return false
+    if (new Set(s).size !== new Set(t).size) return false
+    
     const sMap = new Map(), tMap = new Map()
     
     for (let i = 0; i < s.length; i++) {
-        const tChar = t.charAt(i)
-        const sChar = s.charAt(i)
+        const sChar = s.charAt(i), tChar = t.charAt(i)
         
         if (sMap.get(sChar) !== tMap.get(tChar)) return false
         
         sMap.set(sChar, i + 1)
         tMap.set(tChar, i + 1)
     }
-    
     return true
 };
