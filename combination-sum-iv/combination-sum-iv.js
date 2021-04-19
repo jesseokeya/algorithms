b3afc53678f7e87daf29bb78cc26893013bbb72e
Jesse Okeya <Jesseokeya@gmail.com>
2021-04-19 06:38:36 -0400

    Time: 60 ms (100.00%), Space: 39.2 MB (65.25%) - LeetHub

---
+++ combination-sum-iv/combination-sum-iv.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var combinationSum4 = function(nums, target) {
+   const dp = new Array(target + 1).fill(0)
+   dp[0] = 1
+    
+    for (let i = 1; i <= target; i++) {
+         for (let j = 0; j < nums.length; j++) {
+             if (i - nums[j] >= 0) { 
+                dp[i] += dp[i - nums[j]]
+             }
+         }
+    }
+    
+    return dp[target]
+};
