1cbb092a5ad07fc7704242e238090f92d6250318
Jesse Okeya <Jesseokeya@gmail.com>
2021-10-02 18:48:56 -0400

    Time: 72 ms (86.15%), Space: 39.1 MB (52.98%) - LeetHub

---
+++ two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} numbers
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(numbers, target) {
+    let left = 0, right = numbers.length - 1
+    
+    while (left < right) {
+        const sum = numbers[left] + numbers[right]
+        if (sum > target)  right--
+        else if (sum < target)  left++
+        else {
+            return [left + 1, right + 1]
+        }
+    }
+    
+    return []
+};
