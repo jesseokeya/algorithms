0c76945d88b0c38826cb0ee4b14e1dcc68e5d358
Jesse Okeya <Jesseokeya@gmail.com>
2021-12-01 07:12:18 -0500

    Time: 68 ms (93.04%), Space: 40.5 MB (14.41%) - LeetHub

--- unique-paths/unique-paths.js
+++ unique-paths/unique-paths.js
@@ -4,16 +4,16 @@
  * @return {number}
  */
 var uniquePaths = function(m, n) {
-    const memo = {}
-    
-    const possiblePaths = (m, n) => {
-        const key = `${m},${n}`
-        if (memo.hasOwnProperty(key)) return memo[key]
-        if (m === 1 && n === 1) return 1
-        if (m === 0 || n === 0) return 0
-        memo[key] = possiblePaths(m - 1, n) + possiblePaths(m, n - 1)
-        return memo[key] 
-    }
-    
-    return possiblePaths(m, n)
+    const possiblePaths = (m, n, memo) => {
+        const key = `${m},${n}`
+        if (memo.has(key)) return memo.get(key)
+        if (m === 1 && n === 1) return 1
+        if (m === 0 || n === 0) return 0
+        const result = possiblePaths(m - 1, n, memo) + possiblePaths(m, n - 1, memo)
+        memo.set(key, result)
+        return result
+    }
+    
+    return possiblePaths(m, n, new Map())
+
 };
