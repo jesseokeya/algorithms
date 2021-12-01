9eb93aaac8deaec9699a900368a7f5571d4cb0b0
Jesse Okeya <Jesseokeya@gmail.com>
2021-11-30 23:59:39 -0500

    Time: 76 ms (82.31%), Space: 42.3 MB (48.81%) - LeetHub

--- binary-search/binary-search.js
+++ binary-search/binary-search.js
@@ -4,14 +4,12 @@
  * @return {number}
  */
 var search = function(nums, target) {
-    let left = 0, right = nums.length - 1
-    
-    while (left <= right) {
-        let mid = Math.floor((left + right) / 2)
-        if (nums[mid] === target) return mid
-        else if (nums[mid] < target) left = mid + 1
-        else right = mid - 1
+    let i = 0, j = nums.length - 1
+    while (i <= j) {
+        const mid = Math.floor((i + j) / 2), value = nums[mid]
+        if (value > target) j = mid - 1
+        else if (value < target)  i = mid + 1
+        else return mid
     }
-    
     return -1
 };
