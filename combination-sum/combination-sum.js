45ebe636a30b49a922007828f6fdb3ea7eaa9fd7
Jesse Okeya <Jesseokeya@gmail.com>
2021-11-30 23:43:33 -0500

    Time: 92 ms (69.84%), Space: 41.5 MB (62.95%) - LeetHub

--- combination-sum/combination-sum.js
+++ combination-sum/combination-sum.js
@@ -4,14 +4,18 @@
  * @return {number[][]}
  */
 var combinationSum = function(candidates, target) {
-    const result = []
-    const combination = (prefix, start, sum) => {
-        if (sum === target) return result.push(prefix)
-        for (let i = start; i < candidates.length; i++) {
-            const candidate = candidates[i], total = sum + candidate
-            if (sum <= target) combination(prefix.concat(candidate), i, total)
-        }
-    }
-    combination([], 0, 0)
-    return result
+    const result = []
+    
+    const dfs = (prefix, idx, sum) => {
+        if (target === sum) result.push(prefix)
+        for (let i = idx; i < candidates.length; i++) {
+            const n = candidates[i]
+            if (n + sum > target) continue
+            dfs(prefix.concat(n), i, sum + n)
+        }
+    }
+    
+    dfs([], 0, 0)
+    
+    return result
 };
