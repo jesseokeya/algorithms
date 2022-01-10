de002698d9ebfc58b5e8aeb80262f6702e2a398f
Jesse Okeya <Jesseokeya@gmail.com>
2022-01-10 09:05:21 -0500

    Time: 149 ms (16.66%), Space: 48.7 MB (69.44%) - LeetHub

--- 53-maximum-subarray/53-maximum-subarray.js
+++ 53-maximum-subarray/53-maximum-subarray.js
@@ -3,12 +3,10 @@
  * @return {number}
  */
 var maxSubArray = function(nums) {
-    let runningMax = nums[0], max = nums[0]
-    
-    for (let i = 1; i < nums.length; i++) {
-        runningMax = Math.max(nums[i], nums[i] + runningMax)
+    let runningMax = -Infinity, max = -Infinity
+    for (const num of nums) {
+        runningMax = Math.max(num, num + runningMax)
         max = Math.max(max, runningMax)
     }
-    
     return max
 };
