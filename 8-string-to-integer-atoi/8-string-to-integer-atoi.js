40247f9f338c4e7e0f5a7f01ab0b4dc9bb2aee3a
Jesse Okeya <Jesseokeya@gmail.com>
2022-01-14 07:20:18 -0500

    Time: 91 ms (77.79%), Space: 40.2 MB (92.73%) - LeetHub

--- 8-string-to-integer-atoi/8-string-to-integer-atoi.js
+++ 8-string-to-integer-atoi/8-string-to-integer-atoi.js
@@ -1,34 +1,21 @@
 /**
- * @param {string} s
+ * @param {string} str
  * @return {number}
  */
-var myAtoi = function(s) {
-    const valid = new Set([
-        '.', 
-        '-', 
-        '+', 
-        '0', 
-        '1',  
-        '2', 
-        '3',  
-        '4', 
-        '5', 
-        '6', 
-        '7', 
-        '8', 
-        '9'
-    ])
-    let sanitized = ''
-    for (const char of s) {
-        if (valid.has(char)) sanitized += char
-        if (!valid.has(char) && char !== " " && sanitized.length === 0) return 0
-        if (!valid.has(char) && char !== " " && sanitized.length > 0) break
-        if (char === " " && sanitized.length > 0) break
+var myAtoi = function(str) {
+    const match = str.trim().match(/([\+\-0]*\d+)\D*/)
+    if (!match) return 0
+    const value = Number(match[1])
+    const index = match['index']
+    const min = Math.pow(-2, 31)
+    const max = Math.pow(2, 31) - 1
+    if (value && index === 0) {
+        if (value < min) {
+            return min 
+        } else if (value > max) {
+            return max
+        }
+        return value
     }
-    let result = parseInt(sanitized)
-    result = Number.isNaN(result) ? 0 : result
-    const upperBound = Math.pow(2, 31) - 1, lowerBound = Math.pow(-2, 31)
-    if (result <= lowerBound) return lowerBound
-    if (result >= upperBound) return upperBound
-    return result
+    return 0
 };
