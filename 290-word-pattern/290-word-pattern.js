c93dc1c616377013555807737c99755fe12bcf42
Jesse Okeya <Jesseokeya@gmail.com>
2022-01-17 08:19:08 -0500

    Time: 143 ms (5.29%), Space: 38.4 MB (72.73%) - LeetHub

---
+++ 290-word-pattern/290-word-pattern.js
@@ -0,0 +1,26 @@
+/**
+ * @param {string} pattern
+ * @param {string} s
+ * @return {boolean}
+ */
+var wordPattern = function(pattern, s) {
+    let pointer = 0
+    if (!pattern && !s) return true
+    const words = s.split(" "), hmap = new Map(), seen = new Set()
+    if (words.length !== pattern.length) return false
+    for (const word of words) {
+        const value = pattern.charAt(pointer)
+        if (!hmap.has(word) && !seen.has(value)) {
+            hmap.set(word, value)
+            seen.add(value)
+        }
+        pointer++
+    }
+    let result = true
+    for (let i = 0; i < words.length; i++) {
+        const word = words[i]
+        const char = hmap.get(word) || ""
+        result = result && char === pattern.charAt(i)
+    }
+    return result
+};
