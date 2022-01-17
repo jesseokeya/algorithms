563e9921be111e4a47b568a0ae1f0b7123719717
Jesse Okeya <Jesseokeya@gmail.com>
2022-01-17 08:29:56 -0500

    Time: 87 ms (33.55%), Space: 38.2 MB (85.95%) - LeetHub

--- 290-word-pattern/290-word-pattern.js
+++ 290-word-pattern/290-word-pattern.js
@@ -1,32 +1,26 @@
 /**
  * @param {string} pattern
- * @param {string} s
+ * @param {string} str
  * @return {boolean}
  */
-var wordPattern = function(pattern, s) {
-    const words = s.split(" "), hmap = createMappings(pattern, s, words)
-    if (!pattern && !s) return true
-    if (words.length !== pattern.length) return false
-
-    let result = true
-    for (let i = 0; i < words.length; i++) {
-        const word = words[i]
-        const char = hmap.get(word) || ""
-        result = result && char === pattern.charAt(i)
+var wordPattern = function(pattern, str) {
+    let result = ''
+    const mappings = createMappings(pattern, str)
+    for (let i = 0; i < pattern.length; i++) {
+        result += mappings[pattern.charAt(i)] + " "
     }
-    return result
+    return result.trim() === str
 };
 
-const createMappings = (pattern, s, words) => {
-    let pointer = 0
-    const hmap = new Map(), seen = new Set()
-    for (const word of words) {
-        const value = pattern.charAt(pointer)
-        if (!hmap.has(word) && !seen.has(value)) {
-            hmap.set(word, value)
-            seen.add(value)
+const createMappings = (pattern, str) => {
+    const mappings = {}, values = []
+    const context = str.split(' ')
+    const patterns = pattern.split('')
+    for (let i = 0; i < patterns.length; i++) {
+        if (!mappings[patterns[i]] && !values.includes(context[i])) {
+            values.push(context[i])
+            mappings[patterns[i]] = context[i]
         }
-        pointer++
     }
-    return hmap
+    return mappings
 }
