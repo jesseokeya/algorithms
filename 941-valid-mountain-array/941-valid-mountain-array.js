99c1a3bc5f9358da95caf4232e9ca4313dff4eb2
Jesse Okeya <Jesseokeya@gmail.com>
2022-01-25 08:20:26 -0500

    Time: 146 ms (10.29%), Space: 42.3 MB (10.40%) - LeetHub

---
+++ 941-valid-mountain-array/941-valid-mountain-array.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} arr
+ * @return {boolean}
+ */
+var validMountainArray = function(arr) {
+    if (arr.length < 3) return false
+    
+    let i = 0, j = arr.length - 1
+    while (arr[i] < arr[i + 1]) i++
+    while (arr[j] < arr[j - 1]) j--
+    
+    console.log(i, j)
+    
+    return i === j && i !== 0 && j !== arr.length - 1
+};
