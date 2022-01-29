af7d2f767fb83767b5efa8478aaf46cabfc10a30
Jesse Okeya <Jesseokeya@gmail.com>
2022-01-29 07:29:36 -0500

    Time: 802 ms (19.17%), Space: 52.6 MB (22.46%) - LeetHub

--- 15-3sum/15-3sum.js
+++ 15-3sum/15-3sum.js
@@ -3,23 +3,22 @@
  * @return {number[][]}
  */
 var threeSum = function(nums) {
-    const results = [], seen = new Set()
+    const target = 0, results = [], seen = new Set()
     nums = nums.sort((a, b) => a - b)
     for (let i = 0; i < nums.length; i++) {
         let left = i + 1, right = nums.length - 1
         while (left < right) {
             const sum = nums[i] + nums[left] + nums[right]
-            if (sum > 0) right--
-            else if (sum < 0) left++
+            if (sum > target) right--
+            else if (sum < target) left++
             else {
-                const result = [nums[i], nums[left], nums[right]]
-                const key = result.join("")
+                const key = `${nums[i]},${nums[left]},${nums[right]}`
                 if (!seen.has(key)) {
-                    results.push(result)
+                    results.push([nums[i], nums[left], nums[right]])
                     seen.add(key)
                 }
-                right--
                 left++
+                right--
             }
         }
     }
