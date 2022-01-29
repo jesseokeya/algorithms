47046a70226f4773bfe38541745345de94e07c13
Jesse Okeya <Jesseokeya@gmail.com>
2022-01-29 07:59:13 -0500

    Time: 96 ms (60.30%), Space: 48.6 MB (6.22%) - LeetHub

--- 125-valid-palindrome/125-valid-palindrome.js
+++ 125-valid-palindrome/125-valid-palindrome.js
@@ -6,13 +6,13 @@ var isPalindrome = function(s) {
     s = s.toLowerCase()
     let word = "", start = 97, end = 97 + 25
     for (let i = 0; i < s.length; i++) {
-       const charCode =  s.charCodeAt(i)
-       if (!isNaN(parseInt(s.charAt(i)))) {
-           word += s.charAt(i)
+       const charCode =  s.charCodeAt(i), char = s.charAt(i)
+       if (!isNaN(parseInt(char))) {
+           word += char
            continue
        }
        if (charCode < start || charCode > end) continue
-       word += s.charAt(i)
+       word += char
     }
     
     let result = true
