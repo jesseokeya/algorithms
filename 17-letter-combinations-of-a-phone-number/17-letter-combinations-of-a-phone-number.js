49737c559c036bcd0bd0096bda816b528ea54e2c
Jesse Okeya <Jesseokeya@gmail.com>
2022-01-29 17:54:38 -0500

    Time: 64 ms (96.64%), Space: 42.4 MB (5.43%) - LeetHub

--- 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.js
+++ 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.js
@@ -4,26 +4,29 @@
  */
 var letterCombinations = function(digits) {
     if (!digits.length) return []
-    const combos = {
-        "2": ["a", "b", "c"],
-        "3": ["d", "e", "f"],
-        "4": ["g", "h", "i"],
-        "5": ["j", "k", "l"],
-        "6": ["m", "n", "o"],
-        "7": ["p", "q", "r", "s"],
-        "8": ["t", "u", "v"],
-        "9": ["w", "x", "y", "z"]
-    }, results = []
+    const combos = new Map([
+        ["2", ["a", "b", "c"]],
+        ["3", ["d", "e", "f"]],
+        ["4", ["g", "h", "i"]],
+        ["5", ["j", "k", "l"]],
+        ["6", ["m", "n", "o"]],
+        ["7", ["p", "q", "r", "s"]],
+        ["8", ["t", "u", "v"]],
+        [ "9", ["w", "x", "y", "z"]]
+    ])
+    const results = []
     
     const combine = (prefix, idx) => {
-        if (prefix.length === digits.length) return results.push(prefix) 
-        const values = combos[digits[idx + 1]]
+        if (prefix.length === digits.length) return results.push(prefix)
+        const digit = digits.charAt(idx)
+        const values = combos.get(digit)
         for (let i = 0; i < values.length; i++) {
-            combine(prefix + values[i], idx + 1)
+            const value = values[i]
+            combine(prefix + value, idx + 1)
         }
     }
     
-    combine("", -1)
+    combine("", 0)
     
     return results
 };
