2fe4727352dddde1dc7d31a26f88c2217cbf20fb
Jesse Okeya <Jesseokeya@gmail.com>
2022-01-30 20:03:59 -0500

    Time: 8137 ms (5.04%), Space: 54.6 MB (16.81%) - LeetHub

--- 643-maximum-average-subarray-i/643-maximum-average-subarray-i.js
+++ 643-maximum-average-subarray-i/643-maximum-average-subarray-i.js
@@ -5,13 +5,13 @@
  */
 var findMaxAverage = function(nums, k) {
     if (nums.length === 1) return nums[0]
-    let max = -Infinity
+    let maxAvg = -Infinity
     for (let i = 0; i <= nums.length - k; i++) {
         let sum = 0
         for (let j = 0; j < k; j++) {
             sum += nums[i + j]
         }
-        max = Math.max(max, sum/k)
+        maxAvg = Math.max(maxAvg, sum/k)
     }
-    return max
+    return maxAvg
 };
