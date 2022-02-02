a43299dc37a822a1408eddea7301cfe06e434dab
Jesse Okeya <jesse.okeya@properly.ca>
2022-02-02 07:28:02 -0500

    Time: 98 ms (73.57%), Space: 45.1 MB (29.82%) - LeetHub

---
+++ 695-max-area-of-island/695-max-area-of-island.js
@@ -0,0 +1,34 @@
+/**
+ * @param {number[][]} grid
+ * @return {number}
+ */
+var maxAreaOfIsland = function(grid) {
+    let max = -Infinity
+    
+    for (let i = 0; i < grid.length; i++) {
+        for (let j = 0; j < grid[i].length; j++) {
+            max = Math.max(max, dfs(grid, i, j))
+        }
+    }
+    
+    return max
+};
+
+const dfs = (grid, i, j) => {
+    let islands = 0
+    if (i < 0 || i >= grid.length || j < 0 || j >= grid[0].length || grid[i][j] === 0) {
+        return 0
+    }
+    
+    islands++
+    
+    grid[i][j] = 0
+    
+    islands += dfs(grid, i - 1, j)
+    islands += dfs(grid, i + 1, j)
+    islands += dfs(grid, i, j - 1)
+    islands += dfs(grid, i, j + 1)
+    
+    
+    return islands
+}
