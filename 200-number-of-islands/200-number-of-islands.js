6697874241130ed0653cd9fba7353e3cfa86001a
Jesse Okeya <jesse.okeya@properly.ca>
2022-02-07 20:28:44 -0500

    Time: 154 ms (32.08%), Space: 44.6 MB (52.24%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.js
+++ 200-number-of-islands/200-number-of-islands.js
@@ -4,17 +4,16 @@
  */
 var numIslands = function(grid) {
     let numOfIslands = 0
-    
     for (let i = 0; i < grid.length; i++) {
-        for (let j = 0; j < grid[0].length; j++) {
-            if (grid[i][j] === "1") numOfIslands += dfs(grid, i, j)
-        }
+       for (let j = 0; j < grid[i].length; j++) {
+           numOfIslands += dfs(grid, i, j)
+       } 
     }
-    
     return numOfIslands
 };
 
 const dfs = (grid, i, j) => {
+    let islands = 0
     if (i < 0 || i >= grid.length || j < 0 || j >= grid[0].length || grid[i][j] === "0") {
         return 0
     }
