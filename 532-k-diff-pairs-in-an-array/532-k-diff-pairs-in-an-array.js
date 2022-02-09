4c21e7e69bdadfb8a42fa2f1e123abcbc10df9cd
Jesse Okeya <jesse.okeya@properly.ca>
2022-02-09 08:35:34 -0500

    Time: 64 ms (100.00%), Space: 47.5 MB (16.87%) - LeetHub

--- 532-k-diff-pairs-in-an-array/532-k-diff-pairs-in-an-array.js
+++ 532-k-diff-pairs-in-an-array/532-k-diff-pairs-in-an-array.js
@@ -4,25 +4,34 @@
  * @return {number}
  */
 var findPairs = function(nums, k) {
+    // return 0 when k is less than 0 since absolute difference is never negative
     if (k < 0) return 0
-    const map = new Map()
     
+    // hmap to hold frequency of values
+    const hmap = new Map()
+    
+    // iterate and create frequency count for all values
     for (const num of nums) {
-        if (!map.has(num)) {
-            map.set(num, 1)
+        if (!hmap.has(num)) {
+            hmap.set(num, 1)
         } else {
-            map.set(num, map.get(num) + 1)
+            hmap.set(num, hmap.get(num) + 1)
         }
     }
     
-    let result = 0
-    for (const [key, value] of map) {
-        if (k === 0) {
-            if (value >= 2) result++
-        } else {
-            if (map.has(key + k)) result++
-        }
+    let counter = 0
+    
+    // iterate through map keys if k == 0 and k >= 2 increment counter
+    // since absolute diff of 2 similar numbers will equal to 0
+    // If the key + k is in the map also increment counter as (key + k) - k
+    // will equal the current key 
+    for (const [key, value] of hmap) {
+       if (k === 0) {
+           if (value >= 2) counter++
+       } else {
+           if (hmap.has(key + k)) counter++
+       }
     }
     
-    return result
+    return counter
 };
