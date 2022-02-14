7c2371959b8352b5ec326acfe1fb78eabb4076de
Jesse Okeya <jesse.okeya@properly.ca>
2022-02-14 05:23:40 -0500

    Time: 95 ms (46.38%), Space: 44.1 MB (7.34%) - LeetHub

--- 62-unique-paths/62-unique-paths.js
+++ 62-unique-paths/62-unique-paths.js
@@ -3,14 +3,12 @@
  * @param {number} n
  * @return {number}
  */
-var uniquePaths = function(m, n) {
-    const paths = (i, j, cache) => {
-        const key = `${i},${j}`
-        if (cache.has(key)) return cache.get(key)  
-        if (i === 1 && j === 1) return 1
-        if (i < 1 || j < 1) return 0
-        cache.set(key, paths(i - 1, j, cache) + paths(i, j - 1, cache))
-        return cache.get(key)
-    }
-    return paths(m, n, new Map())
+var uniquePaths = function(m, n, seen = new Map()) {
+    const key = `${m}#${n}`
+    if (seen.has(key)) return seen.get(key)
+    if (m === 1 && n === 1) return 1
+    if (m < 1 || n < 1) return 0
+    const paths = uniquePaths(n - 1, m, seen) + uniquePaths(n, m - 1, seen)
+    seen.set(key, paths)
+    return paths
 };
