70a90984f0c2b4232b36ff910725315b2ae16744
Jesse Okeya <jesse.okeya@properly.ca>
2022-02-14 16:46:55 -0500

    Time: 164 ms (49.03%), Space: 51.2 MB (71.43%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.js
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.js
@@ -4,19 +4,19 @@
  * @return {number}
  */
 var longestCommonSubsequence = function(text1, text2) {
-    if (text1 === text2) return text1.length
-    const lcs = (i, j, memo) => {
-        const key = `${i},${j}`
-        if (memo.has(key)) return memo.get(key)
-        if (i >= text1.length || j >= text2.length) return 0
-        else if (text1[i] === text2[j]) { 
-            const result = 1 + lcs(i + 1, j + 1, memo)
-            memo.set(key, result)
-            return result
+    const row = text1.length, col = text2.length
+    const dp = Array.from({ length: row + 1 }, () => new Uint16Array(col + 1))
+    
+    for (let i = 1; i <= row; i++) {
+        for (let j = 1; j <= col; j++) {
+            if (text1[i - 1] === text2[j - 1]) {
+                dp[i][j] += 1 + dp[i - 1][j - 1]
+            } else {
+                dp[i][j] += Math.max(dp[i - 1][j], dp[i][j - 1])
+            }
         }
-        const result = Math.max(lcs(i + 1, j, memo), lcs(i, j + 1, memo))
-        memo.set(key, result)
-        return result
     }
-    return lcs(0, 0, new Map()) 
+    
+    console.log(dp)
+    return dp[row][col]
 };
