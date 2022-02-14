b39e9c77f3e03386418742eb8b8519ff71abf89e
Jesse Okeya <jesse.okeya@properly.ca>
2022-02-14 05:18:12 -0500

    Time: 68 ms (97.11%), Space: 45 MB (23.92%) - LeetHub

--- 104-maximum-depth-of-binary-tree/104-maximum-depth-of-binary-tree.js
+++ 104-maximum-depth-of-binary-tree/104-maximum-depth-of-binary-tree.js
@@ -14,5 +14,6 @@ var maxDepth = function(root) {
     if (!root) return 0
     const left = maxDepth(root.left)
     const right = maxDepth(root.right)
+    
     return 1 + Math.max(left, right)
 };
