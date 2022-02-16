b09bbc2c9b841bc4d959dbe4b4e5f91c699d9468
Jesse Okeya <jesse.okeya@properly.ca>
2022-02-16 07:59:39 -0500

    Time: 89 ms (65.21%), Space: 46.4 MB (20.82%) - LeetHub

---
+++ 98-validate-binary-search-tree/98-validate-binary-search-tree.js
@@ -0,0 +1,20 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {boolean}
+ */
+var isValidBST = function(root) {
+    const validate = (node, min, max) => {
+        if (!node) return true
+        if (node.val > max || node.val < min) return false
+        return validate(node.left, min, node.val - 1) && validate(node.right, node.val + 1, max)
+    }
+    return validate(root, -Infinity, Infinity)
+};
