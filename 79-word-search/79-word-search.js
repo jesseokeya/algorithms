70fab9b3d365113f9fae12b2badad1e83e591421
Jesse Okeya <jesse.okeya@properly.ca>
2022-02-21 03:01:44 -0500

    Time: 4029 ms (6.36%), Space: 42.3 MB (69.15%) - LeetHub

--- 79-word-search/79-word-search.js
+++ 79-word-search/79-word-search.js
@@ -6,27 +6,32 @@
 var exist = function(board, word) {
     for (let i = 0; i < board.length; i++) {
         for (let j = 0; j < board[i].length; j++) {
-            if (board[i][j] === word.charAt(0) && dfs(board, i, j, 0, word)) return true
+            if (board[i][j] === word[0] && dfs(board, i, j, 0, word)) return true
         }
     }
     return false
 };
 
-const dfs = (grid, i, j, count, word) => {
-    if (count === word.length) return true
-    if (i < 0 || j < 0 || i >= grid.length || j >= grid[i].length || grid[i][j] !== word.charAt(count)) {
+const dfs = (board, i, j, count, word) => {
+    if (word.length === count) return true
+    if (i < 0 || j < 0 
+        || i >= board.length 
+        || j >= board[i].length 
+        || board[i][j] !== word[count]) {
         return false
     }
     
-    const char = grid[i][j]
-    grid[i][j] = "*"
+    const char = board[i][j]
+    board[i][j] = "*"
     
-    const found = dfs(grid, i - 1, j, count + 1, word) 
-        || dfs(grid, i + 1, j, count + 1, word) 
-        || dfs(grid, i, j - 1, count + 1, word) 
-        || dfs(grid, i, j + 1, count + 1, word) 
+    const found = dfs(board, i - 1, j, count + 1, word) 
+        || dfs(board, i + 1, j, count + 1, word)
+        || dfs(board, i - 1, j, count + 1, word)
+        || dfs(board, i, j - 1, count + 1, word) 
+        || dfs(board, i, j + 1, count + 1, word) 
     
-    grid[i][j] = char
-          
+    board[i][j] = char
+    
     return found
 }
+    
