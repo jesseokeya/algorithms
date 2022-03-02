e755e4bb8dcc2c7f6858f19f37af6722435e6939
Jesse Okeya <jesse.okeya@properly.ca>
2022-03-02 04:15:38 -0500

    Time: 60 ms (97.66%), Space: 42.6 MB (11.13%) - LeetHub

---
+++ 392-is-subsequence/392-is-subsequence.js
@@ -0,0 +1,17 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isSubsequence = function(s, t) {
+    let i = 0, j = 0
+    while (j < t.length) {
+        if (s[i] === t[j]) {
+            j++
+            i++
+        } else {
+            j++
+        }
+    }
+    return i === s.length
+};
