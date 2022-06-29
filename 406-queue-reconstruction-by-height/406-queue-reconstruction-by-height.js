c7637517f603e484ab527da09df69a1dd19bac7a
Jesse Okeya <jesse.okeya@properly.ca>
2022-06-29 06:28:48 -0400

    Time: 108 ms (84.37%), Space: 48 MB (36.46%) - LeetHub

--- 406-queue-reconstruction-by-height/406-queue-reconstruction-by-height.js
+++ 406-queue-reconstruction-by-height/406-queue-reconstruction-by-height.js
@@ -4,10 +4,14 @@
  */
 var reconstructQueue = function(people) {
     const result = []
-    const sorted = people.sort((a, b) => a[0] === b[0] ? a[1] - b[1] : b[0] - a[0])
+    const sorted = people.sort(
+        (a, b) => a[0] === b[0] ? a[1] - b[1] : b[0] - a[0]
+    )
+    
     for (const [h, k] of sorted) {
         if (!result[k]) result[k] = [h, k]
         else result.splice(k, 0, [h, k])
     }
+    
     return result
 };
