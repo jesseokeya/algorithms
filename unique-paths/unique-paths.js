8beba890c32ed89452fe1e31d3691042bc726e9f
Jesse Okeya <jesse.okeya@properly.ca>
2022-07-10 16:48:04 -0400

    Time: 75 ms, Memory: 44.3 MB - LeetHub

--- unique-paths/unique-paths.js
+++ unique-paths/unique-paths.js
@@ -3,15 +3,13 @@
  * @param {number} n
  * @return {number}
  */
-var uniquePaths = function(m, n) {
-    const dfs = (m, n, memo) => {
-        const key = `${m},${n}`
-        if (memo.has(key)) return memo.get(key)
-        if (m === 0 && n === 0) return 1
-        if (m < 0 || n < 0) return 0
-        memo.set(key, dfs(m - 1, n, memo) + dfs(m, n - 1, memo))
-        return memo.get(key)
-    }
+var uniquePaths = function(m, n, memo = new Map()) {
+    const key = `${m}#${n}`
+    if (memo.has(key)) return memo.get(key)
     
-    return dfs(m - 1, n - 1, new Map())
+    if (m === 1 && n === 1) return 1
+    if (m < 0 || n < 0) return 0
+    
+    memo.set(key, uniquePaths(m - 1, n, memo) + uniquePaths(m, n - 1, memo))
+    return memo.get(key) 
 };
