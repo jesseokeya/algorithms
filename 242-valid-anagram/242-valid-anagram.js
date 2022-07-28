3d2ff2486674c9dabae913e57ff0a2a54a3abc18
Jesse Okeya <jesse.okeya@properly.ca>
2022-07-27 22:01:44 -0400

    Time: 152 ms (22.93%), Space: 44 MB (59.02%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.js
+++ 242-valid-anagram/242-valid-anagram.js
@@ -5,23 +5,26 @@
  */
 var isAnagram = function(s, t) {
     if (s.length !== t.length) return false
-    const sMap = createMapping(s), tMap = createMapping(t)
+        
+    const sMap = mappings(s)
+    const tMap = mappings(t)
+    
     let result = true
+    
     for (const [k, v] of sMap) {
-        if (!tMap.has(k)) return false
-        result = result && tMap.get(k) === v
+        result = result && v === tMap.get(k)
     }
+    
     return result
 };
 
-const createMapping = (word) => {
-    const results = new Map()
-    for (const char of word) {
-        if (!results.has(char)) {
-            results.set(char, 1)
-        } else {
-            results.set(char, results.get(char) + 1)
-        }
+const mappings = (str) => {
+    const result = new Map()
+    
+    for (const char of str) {
+        if (!result.has(char)) result.set(char, 1)
+        else result.set(char, result.get(char) + 1)
     }
-    return results
+    
+    return result
 }
