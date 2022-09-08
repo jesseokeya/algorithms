baacdc6cb71c4ca1151ae01145e87a1be301455b
Jesse Okeya <jesse.okeya@properly.ca>
2022-09-07 22:11:47 -0400

    Time: 191 ms (13.33%), Space: 50.9 MB (10.65%) - LeetHub

--- 150-evaluate-reverse-polish-notation/150-evaluate-reverse-polish-notation.js
+++ 150-evaluate-reverse-polish-notation/150-evaluate-reverse-polish-notation.js
@@ -3,21 +3,24 @@
  * @return {number}
  */
 var evalRPN = function(tokens) {
-    const specialTokens = new Set(["+", "-", "*", "/"])
-    const operations = {
-        "+": (a, b) => a + b,
-        "-": (a, b) => a - b,
-        "*": (a, b) => a * b,
-        "/": (a, b) => Math.trunc(a / b),
-    }, stack = []
-    for (let i = 0; i < tokens.length; i++) {
-        const value = tokens[i], lastElem = stack[stack.length - 1]
-        if (!specialTokens.has(value)) stack.push(value)
-        else {
-            const fn = operations[value]
-            const first = Number(stack.pop()), second = Number(stack.pop())
-            stack.push(fn(second, first))
+    const operators = new Set(['+', '-', '*', '/'])
+    const operations = new Map([
+        ['+', (a, b) => a + b],
+        ['-', (a, b) => a - b],
+        ['*', (a, b) => a * b],
+        ['/', (a, b) => Math.trunc(a / b)],
+    ]), stack = []
+    
+    for (const token of tokens) {
+        if (!operators.has(token)) {
+            stack.push(parseInt(token))
+        } else {
+            const operation = operations.get(token)
+            const prev = stack.pop(), value = stack.pop()
+            console.log({ prev, value, token })
+            stack.push(operation(value, prev))
         }
     }
+    
     return stack.pop()
 };
