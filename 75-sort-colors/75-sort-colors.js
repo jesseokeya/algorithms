584e94b17aa2fca2d0920083a90e9d737aebb14e
Jesse Okeya <jesse.okeya@properly.ca>
2022-09-15 11:58:01 -0400

    Time: 116 ms (11.18%), Space: 42.1 MB (50.92%) - LeetHub

---
+++ 75-sort-colors/75-sort-colors.js
@@ -0,0 +1,12 @@
+/**
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var sortColors = function(nums) {
+    for (let i = 0; i < nums.length; i++) {
+        for (let j = i + 1; j < nums.length; j++) {
+            if (nums[i] > nums[j]) [nums[i], nums[j]] = [nums[j], nums[i]]
+        }
+    }
+    return nums
+};
