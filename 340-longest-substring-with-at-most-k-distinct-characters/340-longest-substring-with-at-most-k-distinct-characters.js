1349ca38afa74023de95c137c1b8d052acf1022b
Jesse Okeya <jesse.okeya@properly.ca>
2022-09-16 11:41:12 -0400

    Time: 124 ms (63.34%), Space: 44.4 MB (67.20%) - LeetHub

--- 340-longest-substring-with-at-most-k-distinct-characters/340-longest-substring-with-at-most-k-distinct-characters.js
+++ 340-longest-substring-with-at-most-k-distinct-characters/340-longest-substring-with-at-most-k-distinct-characters.js
@@ -4,17 +4,17 @@
  * @return {number}
  */
 var lengthOfLongestSubstringKDistinct = function(s, k) {
-    let windowStart = 0, mappings = new Map(), result = 0
+    let windowStart = 0, mappings = new Map(), result = -Infinity
     
     for (let windowEnd = 0; windowEnd < s.length; windowEnd++) {
         const right = s[windowEnd]
-        if (!mappings.has(right)) mappings.set(right, 0)
+        if (!mappings.has(right)) mappings.set(right, 1)
         mappings.set(right, mappings.get(right) + 1)
         
         while (mappings.size > k) {
             const left = s[windowStart]
             mappings.set(left, mappings.get(left) - 1)
-            if (mappings.get(left) === 0) mappings.delete(left)
+            if (mappings.get(left) <= 1) mappings.delete(left)
             windowStart++
         }
         
