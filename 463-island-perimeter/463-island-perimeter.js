113a8ce58fec110925cf62fa1ba261301f9b91d7
Jesse Okeya <jesse.okeya@properly.ca>
2022-09-23 00:38:14 -0400

    Time: 166 ms (89.78%), Space: 51 MB (30.14%) - LeetHub

--- 463-island-perimeter/463-island-perimeter.js
+++ 463-island-perimeter/463-island-perimeter.js
@@ -9,36 +9,20 @@ var islandPerimeter = function(grid) {
     
     for (let i = 0; i < row; i++) {
         for (let j = 0; j < col; j++) {
-            const cell = grid[i][j]
-            if (cell === 1) result += dfs(grid, i, j)
+            if (grid[i][j] === 1) {
+                const positions = [[-1, 0], [0, -1], [0, 1], [1, 0]]
+                for (const position of positions) {
+                    const x = position[0] + i
+                    const y = position[1] + j
+                    if (isOutofBounds(grid, x, y) || !grid[x][y]) result += 1
+                }
+            }
         }
     }
     
     return result
 };
 
-
 const isOutofBounds = (grid, x, y) => {
-    const row = grid.length, col = grid[0].length
-    
-    return x < 0 || x >= row || y < 0 || y >= col
-}
-
-const dfs = (grid, i, j, seen = new Set()) => {
-    const key = `${i}${j}`
-    
-    // Base case 1: out of bounds
-    if (isOutofBounds(grid, i, j) || seen.has(key)) return 0
-    seen.add(key)
-    
-    const positions = [[-1, 0], [0, -1], [0, 1], [1, 0]]
-    
-    let perimeter = 0
-    
-    for (const position of positions) {
-        const x = position[0] + i, y = position[1] + j
-        if (isOutofBounds(grid, x, y) || grid[x][y] === 0) perimeter += 1
-    }
-    
-    return perimeter
+    return x < 0 || x >= grid.length || y < 0 || y >= grid[0].length
 }
