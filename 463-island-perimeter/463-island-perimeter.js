c66d6cf4ea58d809c2fad6cdd1b13568a69cf02a
Jesse Okeya <jesse.okeya@properly.ca>
2022-09-23 00:43:29 -0400

    Time: 322 ms (21.49%), Space: 51.7 MB (20.84%) - LeetHub

--- 463-island-perimeter/463-island-perimeter.js
+++ 463-island-perimeter/463-island-perimeter.js
@@ -3,26 +3,29 @@
  * @return {number}
  */
 var islandPerimeter = function(grid) {
-    const row = grid.length, col = grid[0].length
-    
     let result = 0
+    const row = grid.length, col = grid[0].length
     
     for (let i = 0; i < row; i++) {
         for (let j = 0; j < col; j++) {
-            if (grid[i][j] === 1) {
-                const positions = [[-1, 0], [0, -1], [0, 1], [1, 0]]
-                for (const position of positions) {
-                    const x = position[0] + i
-                    const y = position[1] + j
-                    if (isOutofBounds(grid, x, y) || !grid[x][y]) result += 1
-                }
-            }
+            if (grid[i][j] === 1) result += countSides(grid, i, j)
         }
     }
     
     return result
 };
 
-const isOutofBounds = (grid, x, y) => {
-    return x < 0 || x >= grid.length || y < 0 || y >= grid[0].length
+const countSides = (grid, i, j) => {
+    let result = 0
+    const row = grid.length, col = grid[0].length
+    const positions = [[-1, 0], [0, -1], [0, 1], [1, 0]]
+    
+    for (const position of positions) {
+        const x = position[0] + i, y = position[1] + j
+        const isOutofBounds = x < 0 || x >= row || y < 0 || y >= col
+        
+        if (isOutofBounds || !grid[x][y]) result += 1
+    }
+    
+    return result
 }
