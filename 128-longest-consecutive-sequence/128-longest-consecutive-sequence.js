5f2a2408902f6d375612f9bae2a746482e4d2936
Jesse Okeya <jesse.okeya@properly.ca>
2022-09-24 07:57:29 -0400

    Time: 153 ms (80.34%), Space: 57.4 MB (76.70%) - LeetHub

--- 128-longest-consecutive-sequence/128-longest-consecutive-sequence.js
+++ 128-longest-consecutive-sequence/128-longest-consecutive-sequence.js
@@ -3,21 +3,18 @@
  * @return {number}
  */
 var longestConsecutive = function(nums) {
-    if (nums.length === 0 || nums === null) return 0
-    
+    if (nums.length === 0 || nums.length === null) return 0
     const set = new Set(nums)
     let max = 0
     
     for (const num of set) {
         if (set.has(num - 1)) continue
-        
-        let currNum = num, count = 1
+        let currNum = num, currCount = 1
         while (set.has(currNum + 1)) {
             currNum++
-            count++
+            currCount++
         }
-        
-        max = Math.max(max, count)
+        max = Math.max(max, currCount)
     }
     
     return max
