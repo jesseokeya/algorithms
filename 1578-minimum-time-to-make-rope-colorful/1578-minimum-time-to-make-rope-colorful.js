b25849cb394c3c0438ad7ed223f215e83dd232ac
Jesse Okeya <jesse.okeya@properly.ca>
2022-10-03 12:02:07 -0400

    Time: 160 ms (46.90%), Space: 52.8 MB (47.59%) - LeetHub

--- 1578-minimum-time-to-make-rope-colorful/1578-minimum-time-to-make-rope-colorful.js
+++ 1578-minimum-time-to-make-rope-colorful/1578-minimum-time-to-make-rope-colorful.js
@@ -6,14 +6,10 @@
 var minCost = function(colors, neededTime) {
     let result = 0
     
-    for (let i = 0; i < colors.length; i++) {
-        while (colors[i] === colors[i + 1]) {
-            if (neededTime[i] < neededTime[i+1]) result += neededTime[i];
-            else {
-                result += neededTime[i+1];
-                neededTime[i+1] = neededTime[i];
-            }
-            i++
+    for (let i = 1; i < colors.length; i++) {
+        if (colors[i] === colors[i - 1]) {
+            result += Math.min(neededTime[i], neededTime[i - 1]) 
+            neededTime[i] = Math.max(neededTime[i], neededTime[i - 1])
         }
     }
     
