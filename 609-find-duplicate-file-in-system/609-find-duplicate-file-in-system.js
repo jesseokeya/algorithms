0b863df19a32b830ef5fdcca767c92811c5f459f
Jesse Okeya <jesse.okeya@properly.ca>
2022-10-03 13:01:54 -0400

    Time: 208 ms (62.12%), Space: 61 MB (56.59%) - LeetHub

--- 609-find-duplicate-file-in-system/609-find-duplicate-file-in-system.js
+++ 609-find-duplicate-file-in-system/609-find-duplicate-file-in-system.js
@@ -7,13 +7,7 @@ var findDuplicate = function(paths) {
     
     for (const path of paths) {
         const filePaths = path.split(" "), pathName = filePaths[0]
-        for (let i = 1; i < filePaths.length; i++) {
-            const file = filePaths[i], splitFile = file.split("(")
-            const filePathName = `${pathName}/${splitFile[0]}`, fileName = splitFile[1].replace(')', '')
-            
-            if (!fileMappings.has(fileName)) fileMappings.set(fileName, [filePathName])
-            else fileMappings.get(fileName).push(filePathName)
-        }
+        for (let i = 1; i < filePaths.length; i++) cacheFiles(i, pathName, filePaths, fileMappings)
     }
     
     for (const [key, value] of fileMappings) {
@@ -22,3 +16,11 @@ var findDuplicate = function(paths) {
     
     return results 
 };
+
+const cacheFiles = (idx, pathName, filePaths, fileMappings) => {
+    const file = filePaths[idx], splitFile = file.split("(")
+    const filePathName = `${pathName}/${splitFile[0]}`, fileName = splitFile[1].replace(')', '')
+
+    if (!fileMappings.has(fileName)) fileMappings.set(fileName, [filePathName])
+    else fileMappings.get(fileName).push(filePathName)
+}
