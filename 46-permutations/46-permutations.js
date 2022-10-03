58021653c6447d2a4b9c141cb237938f9dfd4fdc
Jesse Okeya <jesse.okeya@properly.ca>
2022-10-02 23:14:59 -0400

    Time: 70 ms (98.77%), Space: 45.4 MB (21.68%) - LeetHub

---
+++ 46-permutations/46-permutations.js
@@ -0,0 +1,25 @@
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+var permute = function(nums) {
+    const results = []
+    
+    const permutation = (idx, prefix, seen) => {
+        if (idx > nums.length) return
+        if (prefix.length === nums.length) results.push(prefix)
+        
+        for (let i = 0; i < nums.length; i++) {
+            if (!seen.has(i)) {
+                seen.add(i)
+                const value = nums[i]
+                permutation(i, prefix.concat(value), seen)
+                seen.delete(i)
+            }
+        }
+    }
+    
+    permutation(0, [], new Set())
+    
+    return results
+};
