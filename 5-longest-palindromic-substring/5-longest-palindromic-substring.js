f01556647585aeee37156eeb523e5020b80cfb83
Jesse Okeya <jesse.okeya@properly.ca>
2022-10-04 17:36:23 -0400

    Time: 155 ms (61.46%), Space: 49 MB (24.59%) - LeetHub

--- 5-longest-palindromic-substring/5-longest-palindromic-substring.js
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.js
@@ -1,26 +1,28 @@
 /**
  * @param {string} s
  * @return {string}
+ *
  */
 var longestPalindrome = function(s) {
-    let result = [0, 1]
+    let results = [0, 1]
     
     for (let i = 0; i < s.length; i++) {
-        const even = getPalindrome(i - 1, i, s), odd = getPalindrome(i - 1, i + 1, s)
+        const even = getPalindrome(i - 1, i + 1, s)
+        const odd = getPalindrome(i - 1, i, s)
         
         const localMax = even[1] - even[0] > odd[1] - odd[0] ? even : odd
-        result = result[1] - result[0] > localMax[1] - localMax[0] ? result : localMax
+        results = results[1] - results[0] > localMax[1] - localMax[0] ? results : localMax
     }
     
-    return s.slice(result[0], result[1])
+    const [start, end] = results
+    return s.substring(start, end)
 };
 
 const getPalindrome = (left, right, s) => {
     while (left >= 0 && right < s.length) {
         if (s[left] !== s[right]) break
-        left--
         right++
+        left--
     }
-    
     return [left + 1, right]
 }
