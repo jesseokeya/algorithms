1f8f242429422338792faedb42bccacfda560526
Jesse Okeya <jesse.okeya@properly.ca>
2022-10-05 21:51:55 -0400

    Time: 91 ms (90.48%), Space: 53.2 MB (5.19%) - LeetHub

--- 235-lowest-common-ancestor-of-a-binary-search-tree/235-lowest-common-ancestor-of-a-binary-search-tree.js
+++ 235-lowest-common-ancestor-of-a-binary-search-tree/235-lowest-common-ancestor-of-a-binary-search-tree.js
@@ -13,33 +13,36 @@
  * @return {TreeNode}
  */
 var lowestCommonAncestor = function(root, p, q) {
-    const pPath = getPath(root, p), qPath = getPath(root, q)
+    const pPath = getPaths(root, p), qPath = getPaths(root, q)
     
-    let i = 0, j = 0, lastSeen = null
-    while (i < pPath.length && j < qPath.length) {
-        if (pPath[i] === qPath[j]) lastSeen = pPath[i]
+    let i = 0, j = 0, result = null
+    while (i <= pPath.length && j <= qPath.length) {
+        if (pPath[i] === qPath[j]) result = pPath[i] || qPath[j]
         else break
         i++
         j++
     }
     
-    return lastSeen
+    return result
 };
 
-const getPath = (root, target) => {
+const getPaths = (root, target) => {
     let results = []
     
-    const dfs = (node, prefix) => {
-        if (!node) return
-        prefix.push(node)
+    const dfs = (node, values) => {
+        if (!node) return []
+        
+        values.push(node)
+        
         if (node === target) {
-            results = prefix.slice()
+            results = values.slice()
             return
         }
-        dfs(node.left, prefix)
-        dfs(node.right, prefix)
-        prefix.pop()
+    
+        dfs(node.left, values)
+        dfs(node.right, values)
         
+        values.pop()
     }
     
     dfs(root, [])
