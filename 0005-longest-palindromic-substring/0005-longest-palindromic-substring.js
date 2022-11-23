b597a0c12d27617b915f77de615b933c765f96e8
Jesse Okeya <jesse.okeya@properly.ca>
2022-11-23 08:26:06 -0500

    Time: 82 ms (97.75%), Space: 48.2 MB (38.10%) - LeetHub

---
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.js
@@ -0,0 +1,28 @@
+/**
+ * @param {string} s
+ * @return {string}
+ *
+ */
+var longestPalindrome = function(s) {
+    let results = [0, 1]
+    
+    for (let i = 0; i < s.length; i++) {
+        const even = getPalindrome(i - 1, i + 1, s)
+        const odd = getPalindrome(i - 1, i, s)
+        
+        const localMax = even[1] - even[0] > odd[1] - odd[0] ? even : odd
+        results = results[1] - results[0] > localMax[1] - localMax[0] ? results : localMax
+    }
+    
+    const [start, end] = results
+    return s.substring(start, end)
+};
+
+const getPalindrome = (left, right, s) => {
+    while (left >= 0 && right < s.length) {
+        if (s[left] !== s[right]) break
+        right++
+        left--
+    }
+    return [left + 1, right]
+}
